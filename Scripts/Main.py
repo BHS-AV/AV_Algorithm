cdf592bbd1e90bd5896d7acef4913720e6220f5b
babusid <dillemic000@gmail.com>
2019-03-25 14:06:36 -0400

    deletes contents of folder

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -24,6 +24,17 @@ lrender=0
 dt=0
 lastsave=time.time()
 imgnum=0
+import os, shutil
+folder = '/home/racecar/PycharmProjects/AV_Algorithm/Scripts/track'
+for the_file in os.listdir(folder):
+    file_path = os.path.join(folder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+        #elif os.path.isdir(file_path): shutil.rmtree(file_path)
+    except Exception as e:
+        print(e)
+
 while not rospy.is_shutdown():
     #m.update(Lidar.getOrient())
     if (rospy.get_time()>lrender+2.5 and Lidar.isGoingStraight()):
